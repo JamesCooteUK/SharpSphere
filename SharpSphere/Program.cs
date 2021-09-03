b6f4d640028fcf5940a395ab5b87f958db91021e
LancePendergrass <lance.pendergrass@gmail.com>
2021-09-03 10:30:13 -0500

    message size quota increase

--- SharpSphere/Program.cs
+++ SharpSphere/Program.cs
@@ -19,6 +19,7 @@ namespace SharpSphere
 
     internal class Program
     {
+        private static const int MAX_MESSAGE_SIZE = 2147483647;
         private static ManagedObjectReference guestFileManager;
         private static VimPortTypeClient vim;
         private static ManagedObjectReference vm;
@@ -736,6 +737,14 @@ namespace SharpSphere
 
         public static void Main(string[] args)
         {
+            binding.MaxBufferPoolSize = MAX_MESSAGE_SIZE;
+            binding.MaxBufferSize = MAX_MESSAGE_SIZE;
+            binding.MaxReceivedMessageSize = MAX_MESSAGE_SIZE;
+            binding.ReaderQuotas.MaxStringContentLength = MAX_MESSAGE_SIZE;
+            binding.ReaderQuotas.MaxArrayLength = MAX_MESSAGE_SIZE;
+            binding.ReaderQuotas.MaxDepth = MAX_MESSAGE_SIZE;
+            binding.ReaderQuotas.MaxBytesPerRead = MAX_MESSAGE_SIZE;
+            
             Parser.Default.ParseArguments<DumpOptions, ListOptions, ExecuteOptions, C2Options, UploadOptions, DownloadOptions>(args).MapResult(
                 (DumpOptions options) => Dump(options),
                 (ListOptions options) => List(options),
